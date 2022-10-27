e63990c4dd0508504f4fe6a57156d18eeafa65d1
Kamyk0417 <104413024+Kamyk0417@users.noreply.github.com>
2022-10-27 13:28:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-def f1(func):
-    def wrapper():
-        print("Start")
-        func()
-        print("Stop")
-    return wrapper
+import player, enemies
+from random import randint
 
-@f1
-def f():
-    print("Hello")
+print("Welcome to the game\n\n")
+name = input("Name your hero: ")
+p1 = player.Player(name)
 
-print(f())
+game_over = False
+
+while not game_over:
+    g1 = enemies.Goblin()
+    while g1.alive:
+        g1.appear(randint(1, p1.level))
+        print("Roll the dice to increase your speed")
+        p_dmg_2 = p1.stats["agility"] + randint(1, 6)
+        e_dmg_2 = g1.speed + randint(1, 6)
+        print(f"Your speed - {p_dmg_2}, enemy's - {e_dmg_2}")
+
+
+
+
+
+
+
