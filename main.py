021c2e7bd844a85c23fbef897ce2fdce8a0e8c57
Kamyk0417 <104413024+Kamyk0417@users.noreply.github.com>
2022-11-03 13:24:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import player, enemies
-from random import randint
+import events
 
-print("Welcome to the game\n\n")
-name = input("Name your hero: ")
-p1 = player.Player(name)
+print("Welcome to the game\n")
+#name = input("Name your hero: ")
+p1 = player.Player("name")
+p1.show_stats()
 
-game_over = False
-
-while not game_over:
-    g1 = enemies.Goblin()
-    while g1.alive:
-        g1.appear(randint(1, p1.level))
-        print("Roll the dice to increase your speed")
-        p_dmg_2 = p1.stats["agility"] + randint(1, 6)
-        e_dmg_2 = g1.speed + randint(1, 6)
-        print(f"Your speed - {p_dmg_2}, enemy's - {e_dmg_2}")
-
-
-
+#while not p1.game_over:
+g1 = enemies.Goblin()
+events.duel(p1,g1,1)
 
 
 
