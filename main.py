1acc3aaebb8fb5647cfbac470a002b90b6fbd49c
Kamyk0417 <104413024+Kamyk0417@users.noreply.github.com>
2023-04-13 20:46:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import player, enemies
 import events
 
 print("Welcome to the game\n")
-#name = input("Name your hero: ")
+name = input("Name your hero: ")
 p1 = player.Player("name")
 p1.show_stats()
 
-#while not p1.game_over:
+while not p1.game_over:
 g1 = enemies.Goblin()
 events.duel(p1,g1,1)
 
